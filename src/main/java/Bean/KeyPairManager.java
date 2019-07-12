28d6bfefee6a0d6fd8f3dd6677fac104457f9051
guocqian <1294201528@qq.com>
2019-07-12 09:34:31 +0800

    Initial commit

---
+++ src/main/java/Bean/KeyPairManager.java
@@ -0,0 +1,15 @@
+package Bean;
+
+import java.security.KeyPair;
+
+public class KeyPairManager {
+    static KeyPair k;
+
+    static public KeyPair getK() {
+        return k;
+    }
+
+    static public void setK(KeyPair k1) {
+        k = k1;
+    }
+}
